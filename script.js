c8faff172e05f8102209fb3557a28e4c3f1da613
Mariusz <casmar90@gmail.com>
2023-03-08 12:45:09 +0100

    Add grid, content, media queries

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ hamburger.addEventListener('click', () => {
 
 // Hide sidebar on less than 1024px after click
 document.addEventListener('click', (e) => {
-    const mediaQuery = window.matchMedia('(max-width: 1023px)');
-    if (mediaQuery.matches && !hamburger.contains(e.target)) {
+    if (window.matchMedia('(max-width: 1023px)').matches && !hamburger.contains(e.target)) {
         wrapper.classList.remove('sidebar-toggle');
         nav.classList.remove('sidebar-toggle');
     }
@@ -25,8 +24,7 @@ window.addEventListener('resize', () => {
 
 // Show sidebar on 1024px and higher, also after resize
 function showSidebar() {
-    const mediaQuery = window.matchMedia('(min-width: 1024px)');
-    if (mediaQuery.matches) {
+    if (window.matchMedia('(min-width: 1024px)').matches) {
         wrapper.classList.add('sidebar-toggle');
         nav.classList.add('sidebar-toggle');
     }
